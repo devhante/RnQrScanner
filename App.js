d1284ea2deedd5a9069f51049f2fa39032fc1fbf
Kyungwook Cho <devhante@gmail.com>
2020-08-24 09:46:56 +0900

    message 통신 구현

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const App = () => {
-  return <></>;
-};
+  const onMessage = (event) => {
+    console.log(event.nativeEvent.data);
+    this.wv.postMessage(event.nativeEvent.data);
+  };
 
-const styles = StyleSheet.create({});
+  const nfc = () => {};
+
+  return (
+    <WebView
+      ref={(wv) => (this.wv = wv)}
+      source={{uri: 'http://spacekorea.kr:1049/rn-hardware-api/'}}
+      onMessage={onMessage}
+    />
+  );
+};
 
 export default App;
